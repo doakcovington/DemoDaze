37c9650b41c00ebcffdb044fb296d297a5603a09
doakcovington <doakcovington@protonmail.com>
2020-05-20 15:17:22 -0700

    Added user_id to bikes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_213642) do
+ActiveRecord::Schema.define(version: 2020_05_20_221541) do
 
   create_table "bikes", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_05_20_213642) do
     t.integer "dealer_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
     t.index ["dealer_id"], name: "index_bikes_on_dealer_id"
   end
 
