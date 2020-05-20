c5cc18337804122bff4a1133ad762653f05b5d36
doakcovington <doakcovington@protonmail.com>
2020-05-20 16:06:20 -0700

    validates email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_many :demo_reports
     has_many :bikes, through: :demo_reports
+    validates :email, uniqueness: true, presence: true
 
     has_secure_password
 end
