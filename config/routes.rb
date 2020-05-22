9be3a1d9cb1eab3662e249fae9f4b20eeadd3879
doakcovington <doakcovington@protonmail.com>
2020-05-21 20:33:54 -0700

    Fixed logout route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   
-  post '/logout' => 'sessions#destroy' #Don't use get for logout
+  delete '/logout' => 'sessions#destroy' #Don't use get for logout
 
   
   
