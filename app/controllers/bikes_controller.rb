853646ef3407f447ce15c7052f47705bff43d1bd
doakcovington <doakcovington@protonmail.com>
2020-05-22 18:58:09 -0700

    Built out bikescontroller methods

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,2 +1,17 @@
 class BikesController < ApplicationController
+    def index
+
+    end
+
+    def show
+
+    end
+
+    def new
+        @bike = Bike.new
+    end
+
+    def create
+
+    end
 end
