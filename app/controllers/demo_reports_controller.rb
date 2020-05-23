db442586d813a648a16c9245ce6d2bf59af8d51f
doakcovington <doakcovington@protonmail.com>
2020-05-22 18:56:58 -0700

    Built out methods for DemoReportsController

--- app/controllers/demo_reports_controller.rb
+++ app/controllers/demo_reports_controller.rb
@@ -1,2 +1,17 @@
 class DemoReportsController < ApplicationController
+    def index
+
+    end
+
+    def show
+
+    end
+
+    def new
+        @demo_report = DemoReport.new
+    end
+
+    def create
+
+    end
 end
