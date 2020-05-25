6e34f93f79dc00db6d8dca369bf3f1b7b91562e5
doakcovington <doakcovington@protonmail.com>
2020-05-24 20:57:02 -0700

    A user can now create a demo_report

--- app/controllers/demo_reports_controller.rb
+++ app/controllers/demo_reports_controller.rb
@@ -4,7 +4,7 @@ class DemoReportsController < ApplicationController
     end
 
     def show
-
+        @demo_report = DemoReport.find_by_id(params[:id])
     end
 
     def new
@@ -12,9 +12,20 @@ class DemoReportsController < ApplicationController
         @demo_report = @bike.demo_reports.build
     end
 
+    def create
+        @demo_report = DemoReport.new(demo_report_params)
+        @demo_report.user_id = session[:user_id]
+        if @demo_report.save
+            redirect_to demo_report_path(@demo_report)
+        else
+            #raise params.inspect
+            render :new
+        end
+    end
+
     private
 
-    def set_bike
-        @bike = Bike.find_by_id(params[:bike_id])
+    def demo_report_params
+        params.require(:demo_report).permit(:bike_id, :location, :rental_date, :return_date, :report)
     end
 end
--- app/models/demo_report.rb
+++ app/models/demo_report.rb
@@ -1,4 +1,6 @@
 class DemoReport < ApplicationRecord
   belongs_to :user
   belongs_to :bike
+
+  validates :report, presence: true 
 end
