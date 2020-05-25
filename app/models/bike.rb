36463fd92c50608c2091dfe021b51397086563e6
doakcovington <doakcovington@protonmail.com>
2020-05-25 12:12:45 -0700

    Fixed error partial bug

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -3,4 +3,7 @@ class Bike < ApplicationRecord
   has_many :demo_reports
   has_many :users, through: :demo_reports
   accepts_nested_attributes_for :dealer
+
+  validates :name, presence: true
+  validates :dealer, presence: true
 end
