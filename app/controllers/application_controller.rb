805574e9b620e2f7eae4022b7bb57fa95df35399
doakcovington <doakcovington@protonmail.com>
2020-05-25 18:50:55 -0700

    Created logged_in helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
 
     private
 
+    def logged_in?
+        !!current_user
+    end
+
     def current_user
         @current_user ||= User.find_by_id(session[:id])
     end
