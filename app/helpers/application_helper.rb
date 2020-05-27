b340fec8cc3699846df07dad00be6e5b162d3a02
doakcovington <doakcovington@protonmail.com>
2020-05-26 19:31:47 -0700

    Created nav bar helper methods and partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+    
+    def logged_in?
+        !!current_user
+    end
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:id]) if session[:user_id]
+    end
+
+    def nav_bar
+        # if logged_in?
+            render partial: '/layouts/logged_in'
+       # else
+            #render partial: '/layouts/logged_out'
+        # end
+    end
 end
