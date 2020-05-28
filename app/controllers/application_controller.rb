bcc35a22c69017e2f492e7f83f4d3bc1ac7a35f3
doakcovington <doakcovington@protonmail.com>
2020-05-27 19:33:39 -0700

    Added redirect_if_not_logged_in helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
     end
 
     def current_user
-        @current_user ||= User.find_by_id(session[:id])
+        @current_user ||= User.find_by(id: session[:id]) if session[:user_id]
+    end
+
+    def redirect_if_not_logged_in
+        redirect_to '/' if !logged_in? #redirects to home page if logged_in? is false
     end
 end
