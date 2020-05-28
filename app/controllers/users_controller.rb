74bae495ca0a59ac1820040384d2487dd3b6f0fd
doakcovington <doakcovington@protonmail.com>
2020-05-27 19:34:14 -0700

    Can't access user show page if not logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save #if true then log in user
-            session[:user_id] = @user.id
+            session[:user_id] = @user.id #adds the user to the session
             redirect_to user_path(@user)
         else #redirect to login page
             render :new
@@ -14,13 +14,14 @@ class UsersController < ApplicationController
     end
 
     def show
+        redirect_if_not_logged_in
         @user = User.find_by_id(params[:id]) #find_by_id allows redirect if there is no id
         redirect_to '/' if !@user #redirects to index if there is no user
     end
 
     private
 
-    def user_params
+    def user_params #strong params
         params.require(:user).permit(:name, :email, :password)
     end
 end
