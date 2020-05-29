7e68bbdbaab5fac7321cd2010b21ad05ce4904f9
doakcovington <doakcovington@protonmail.com>
2020-05-28 19:43:04 -0700

    Fixed current_user bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
     
-    # helper_method :current_user, :logged_in? #gives access to helper methods in views
+     helper_method :current_user, :logged_in? #gives access to helper methods in views
 
-    private
 
     def logged_in?
         !!session[:user_id]
     end
-
+    
     def current_user
-        @current_user ||= User.find_by(id: session[:id]) if session[:user_id]
+        @current_user ||= User.find_by(id: session[:user_id])
     end
+    
 
     def redirect_if_not_logged_in
         redirect_to '/' if !logged_in? #redirects to home page if logged_in? is false
