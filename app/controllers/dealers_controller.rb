8319778975a0690c23b610f3667c01284cee50a2
doakcovington <doakcovington@protonmail.com>
2020-06-23 18:30:46 -0700

    Created dealer show page

--- app/controllers/dealers_controller.rb
+++ app/controllers/dealers_controller.rb
@@ -2,4 +2,13 @@ class DealersController < ApplicationController
     def index
         @dealers = Dealer.alpha #sorts delears in alphabetical order
     end
+
+    def show
+        if  @dealer = Dealer.find_by_id(params[:id]) #if demo report exists
+            @dealer #display demo report page
+       else
+            flash[:message] = "That bike doesn't exist"
+            redirect_to bikes_path #redirect to demo report index page
+       end
+    end
 end
