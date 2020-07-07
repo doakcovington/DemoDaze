5ad1d48edd883f9904f7ec42aee4164cdc9c09d2
doakcovington <doakcovington@protonmail.com>
2020-07-07 16:19:39 -0700

    Created and implemented custom validation

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -5,10 +5,18 @@ class Bike < ApplicationRecord
   accepts_nested_attributes_for :dealer
 
   validates :name, presence: true
+  validate :dealer_bike_uniquness
 
 
   def dealer_attributes=(attributes) #reader method for dealer nested form inside new bike form
     self.dealer = Dealer.find_or_create_by(attributes) if !attributes['name'].empty?
     self.dealer
+  end
+
+  def dealer_bike_uniquness
+    #if a bike with that name already belongs to a dealer, don't create a new bike object
+    if Bike.find_by(name: name, dealer_id: dealer_id) 
+      errors.add(:name, "This bike already exists for this dealer. You can write a demo report for this bike on the bike page!")
+    end
   end
 end
